bbe36b7b459e001a6b86bdfce6ad0cae6ecfd857
1k10 <77734986+app155@users.noreply.github.com>
2025-12-22 14:48:45 +0900

    20251222
    20251222

--- ServletProject/src/main/java/com/boardone/BoardDAO.java
+++ ServletProject/src/main/java/com/boardone/BoardDAO.java
@@ -47,7 +47,7 @@ public class BoardDAO {
 			}
 			
 			if (num != 0) {
-				sql = "update set step = step + 1 where ref = ? and step > ?";
+				sql = "update board set step = step + 1 where ref = ? and step > ?";
 				pstmt = con.prepareStatement(sql);
 				pstmt.setInt(1, ref);
 				pstmt.setInt(2, step);
@@ -165,6 +165,59 @@ public class BoardDAO {
 		return x;
 	}
 	
+	// 검색조건으로 검색한 내용의 개수 반환
+	public int getArticleCount(String what, String content) {
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		int x = 0;
+		
+		try {
+			con = ConnUtil.getConnection();
+			String sql = "select count(*) from board where " + what + " like '%" + content + "%'";
+			pstmt = con.prepareStatement(sql);
+			rs = pstmt.executeQuery();
+			
+			if (rs.next()) {
+				x = rs.getInt(1);
+			}
+		}
+		catch (Exception se) {
+			se.printStackTrace();
+		}
+		
+		finally {
+			if (rs != null) {
+				try {
+					rs.close();
+				}
+				catch (SQLException ss) {
+					ss.printStackTrace();
+				}
+			}
+			
+			if (pstmt != null) {
+				try {
+					pstmt.close();
+				}
+				catch (SQLException ss) {
+					ss.printStackTrace();
+				}
+			}
+			
+			if (con != null) {
+				try {
+					con.close();
+				}
+				catch (SQLException ss) {
+					ss.printStackTrace();
+				}
+			}
+		}
+		
+		return x;
+	}
+	
 	// db에서 데이터를 가져와 list에 저장
 	public List<BoardVO> getArticles(int start, int end) {
 		Connection con = null;
@@ -178,6 +231,82 @@ public class BoardDAO {
 			String sql = "select * from "
 					+ "(select rownum rnum, num, writer, email, subject, pass, regdate, readcount, ref, step, depth, content, ip from "
 					+ "(select * from board order by ref desc, step asc)) where rnum >= ? and rnum <= ?";
+			pstmt = con.prepareStatement(sql);
+			pstmt.setInt(1, start);
+			pstmt.setInt(2, end);
+			rs = pstmt.executeQuery();
+			
+			if (rs.next()) {
+				articleList = new ArrayList<BoardVO>(end - start - 1);
+				
+				do {
+					BoardVO article = new BoardVO();
+					article.setNum(rs.getInt("num"));
+					article.setWriter(rs.getString("writer"));
+					article.setEmail(rs.getString("email"));
+					article.setSubject(rs.getString("subject"));
+					article.setPass(rs.getString("pass"));
+					article.setReadcount(rs.getInt("readcount"));
+					article.setRegdate(rs.getTimestamp("regdate"));
+					article.setRef(rs.getInt("ref"));
+					article.setStep(rs.getInt("step"));
+					article.setDepth(rs.getInt("depth"));
+					article.setContent(rs.getString("content"));
+					article.setIp(rs.getString("ip"));
+					
+					articleList.add(article);
+				} while (rs.next());
+			}
+		}
+		catch (Exception se) {
+			se.printStackTrace();
+		}
+		
+		finally {
+			if (rs != null) {
+				try {
+					rs.close();
+				}
+				catch (SQLException ss) {
+					ss.printStackTrace();
+				}
+			}
+			
+			if (pstmt != null) {
+				try {
+					pstmt.close();
+				}
+				catch (SQLException ss) {
+					ss.printStackTrace();
+				}
+			}
+			
+			if (con != null) {
+				try {
+					con.close();
+				}
+				catch (SQLException ss) {
+					ss.printStackTrace();
+				}
+			}
+		}
+		
+		return articleList;
+	}
+	
+	// 검색내용들을 리스트로 반환
+	public List<BoardVO> getArticles(String what, String content, int start, int end) {
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		List<BoardVO> articleList = null;
+		
+		try {
+			con = ConnUtil.getConnection();
+			//String sql = "select * from board order by num desc";
+			String sql = "select * from "
+					+ "(select rownum rnum, num, writer, email, subject, pass, regdate, readcount, ref, step, depth, content, ip from "
+					+ "(select * from board where " + what + " like '%" + content + "%' order by ref desc, step asc)) where rnum >= ? and rnum <= ?";
 			pstmt = con.prepareStatement(sql);
 			pstmt.setInt(1, start);
 			pstmt.setInt(2, end);
--- ServletProject/src/main/webapp/boardone/list.jsp
+++ ServletProject/src/main/webapp/boardone/list.jsp
@@ -9,6 +9,13 @@
 
 	int pageSize = 5;
 	String pageNum = request.getParameter("pageNum");
+	String searchWhat = request.getParameter("searchWhat");
+	String searchText = request.getParameter("searchText");
+	
+	// 받아온 파라미터 값 변환처리
+	if (searchText != null) {
+		searchText = new String(searchText.getBytes("UTF-8"), "UTF-8");
+	}
 	
 	if (pageNum == null) {
 		pageNum = "1";
@@ -21,11 +28,23 @@
 	int number = 0;
 	List<BoardVO> articleList = null;
 	BoardDAO dbPro = BoardDAO.getInstance();
-	count = dbPro.getArticleCount();
 	
-	if (count > 0) {
-		articleList = dbPro.getArticles(startRow, endRow);
+	// 검색 아닐 시 전체 리스트 출력, 검색일시 검색조건에 해당하는 리스트만 출력
+	if (searchText == null) {
+		count = dbPro.getArticleCount();
+		
+		if (count > 0) {
+			articleList = dbPro.getArticles(startRow, endRow);
+		}
 	}
+	else {
+		count = dbPro.getArticleCount(searchWhat, searchText);
+		
+		if (count > 0) {
+			articleList = dbPro.getArticles(searchWhat, searchText, startRow, endRow);
+		}
+	}
+	
 
 	number = count - (currentPage - 1) * pageSize;
 %>
@@ -125,24 +144,54 @@
 				endPage = pageCount;
 			}
 			
+			// 검색일 경우, 아닐경우 페이징 처리 구분
 			if (startPage > pageBlock) {
+				if (searchText == null) {
 	%>
 	<a href="list.jsp?pageNum=<%=startPage - pageBlock %>">[이전]</a>
-	
 	<%
+				}
+				else {
+	%>
+	<a href="list.jsp?pageNum=<%=startPage - pageBlock %>&searchWhat=<%=searchWhat %>&searchText=<%=searchText %>">[이전]</a>
+	<%
+				}
 			}
 			for (int i = startPage; i <= endPage; i++) {
+				if (searchText == null) {
 	%>
 	<a href="list.jsp?pageNum=<%=i %>">[<%=i %>]</a>
 	<%
+				}
+				else {
+	%>
+	<a href="list.jsp?pageNum=<%=i %>&searchWhat=<%=searchWhat %>&searchText=<%=searchText %>">[<%=i %>]</a>
+	<%
+				}
 			}
 			if (endPage < pageCount) {
+				if (searchText == null) {
 	%>
 	<a href="list.jsp?pageNum=<%=startPage + pageBlock %>">[다음]</a>
+	<% 
+				}
+				else {
+	%>
+	<a href="list.jsp?pageNum=<%=startPage + pageBlock %>&searchWhat=<%=searchWhat %>&searchText=<%=searchText %>">[다음]</a>
 	<%
+				}
 			}
 		}
 	%>
+	<form action="list.jsp" method="post">
+		<select name="searchWhat">
+			<option value="writer">작성자</option>
+			<option value="subject">제목</option>
+			<option value="content">내용</option>
+		</select>
+		<input type="text" name="searchText">
+		<input type="submit" value="검색">
+	</form>
 </div>
 </body>
 </html>
